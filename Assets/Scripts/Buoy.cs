39201f18bf98046c82e652c12f4ab63da541a5d3
Zuzanna Lewandowska <miniaturkazdl@gmail.com>
2026-05-09 20:16:17 +0200

    Laser Beam

---
+++ Assets/Scripts/Buoy.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Buoy : MonoBehaviour
+{
+    public static List<Buoy> allBuoys = new List<Buoy>();
+
+    public float safeRadius = 25f;
+    public float fogPushDuration = 10f;  
+    private bool isUsed = false;
+
+    void OnEnable() { allBuoys.Add(this); }
+    void OnDisable() { allBuoys.Remove(this); }
+
+    public float Activate()
+    {
+        if (isUsed) return 0;
+        isUsed = true;
+         
+        if (BuoySequenceManager.Instance != null)
+        {
+            BuoySequenceManager.Instance.SpawnNextBuoy();
+        }
+
+        Destroy(gameObject, 0.1f);
+        return fogPushDuration;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, safeRadius);
+    }
+}
